9fdf57af9f33fc6a7b3e2c53c497df722044376f
mayarmahmoud80 <mahmoudmayar699@yahoo.com>
2026-02-16 18:33:02 +0200

    update

--- prisma/migrations/20260216152343_init/migration.sql
+++ prisma/migrations/20260216152343_init/migration.sql
@@ -1,9 +1,14 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('ADMIN', 'LEGAL', 'BUSINESS', 'CLIENT');
+
 -- CreateTable
 CREATE TABLE "User" (
     "id" SERIAL NOT NULL,
     "name" TEXT NOT NULL,
     "email" TEXT NOT NULL,
+    "role" "Role" NOT NULL DEFAULT 'BUSINESS',
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
